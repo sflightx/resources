3cc3688a19520c6fbac752efc2d4eae444eb0086
sFlightX <sflightx@gmail.com>
2024-05-18 23:41:37 +0800

    update 2

---
+++ js/sflightx/sflightx.js
@@ -0,0 +1,17 @@
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.querySelector("header").style.top = "0";
+  } else {
+    document.querySelector("header").style.top = "-7.2rem";
+  }
+  prevScrollpos = currentScrollPos;
+
+  // Change background if scroll is beyond 100vh
+  if (currentScrollPos > window.innerHeight) {
+    document.querySelector("header").style.backgroundColor = "rgba(0,0,0,1)";
+  } else {
+    document.querySelector("header").style.backgroundColor = "rgba(0,0,0,0)";
+  }
+}
