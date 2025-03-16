6b73edc847fea2e8543fe66fc7c6795c28c28d17
sflightx <spaceflightxploration@gmail.com>
2025-03-16 21:58:04 +0800

    run

---
+++ data/athena/getContractDetails.js
@@ -0,0 +1,29 @@
+const serviceAccount = 'ttps://sflightx.com/resources/serviceAuth/credential.json';
+const db = firebase.database();
+const app = firebase.initializeApp(serviceAccount);
+if (!firebase.apps.length) {
+    console.log("Initializing Firebase...");
+    const auth = firebase.auth(app);
+} else {
+    console.log("Firebase already initialized.");
+    firebase.app();
+}
+
+document.addEventListener('DOMContentLoaded', async () => {
+    async function getContractDetails(key) {
+        try {
+            const ref = db.ref(key);
+            const snapshot = await ref.once('value');
+            const data = snapshot.val();
+            console.log(data);
+            return data;
+        } catch (error) {
+            console.error('Error retrieving data:', error);
+            throw error;
+        }
+    }
+
+    // Example usage
+    const pathKey = 'path/to/your/data';
+    await getContractDetails(pathKey);
+});
--- main/init.js
+++ main/init.js
@@ -1,26 +1,4 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyATiu2dMGbYxGYH0S_31J8XsK5OBpTLMfQ",
-        authDomain: "sflight-x.firebaseapp.com",
-        databaseURL: "https://sflight-x-default-rtdb.firebaseio.com",
-        projectId: "sflight-x",
-        storageBucket: "sflight-x.appspot.com",
-        messagingSenderId: "219145189221",
-        appId: "1:219145189221:web:4a94931b99d15659fe138c",
-        measurementId: "G-813MEL2GZF"
-    };
-    // Initialize Firebase
-    if (!firebase.apps.length) {
-        // Initialize Firebase
-        console.log("Initializing Firebase...");
-        const app = firebase.initializeApp(firebaseConfig);
-        const auth = firebase.auth(app);
-    } else {
-        // Use the existing Firebase app
-        console.log("Firebase already initialized.");
-        firebase.app();
-    }
-
     var prevScrollpos = window.pageYOffset;
     window.onscroll = function() {
         setTimeout(function() {
