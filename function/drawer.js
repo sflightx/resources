adc6f9d6c301177caf1f2f3f6622273eb3649912
sflightx <spaceflightxploration@gmail.com>
2025-04-23 14:31:50 +0800

    Implement code changes to enhance functionality and improve performance

--- function/drawer.js
+++ function/drawer.js
@@ -1,6 +1,7 @@
 
 // Elements
 const drawer = document.getElementById("drawer");
+const side_sheet = document.getElementById("side-sheet");
 const backdrop = document.getElementById("backdrop");
 
 // Drawer toggle function
@@ -9,5 +10,11 @@ function toggleDrawer() {
     backdrop.classList.toggle("hidden");   // Show/hide backdrop
 }
 
+function toggleSideSheet() {
+    side_sheet.classList.toggle("open");       // Show/hide drawer
+    backdrop.classList.toggle("hidden");   // Show/hide backdrop
+}
+
 // Expose to HTML onclick
 window.toggleDrawer = toggleDrawer;
+window.toggleSideSheet = toggleSideSheet;
