8d224f5838ae045d68315955dadb27edaedcdceb
sflightx <spaceflightxploration@gmail.com>
2025-04-25 10:57:42 +0800

    Update download button layout to use flexbox for improved spacing

--- function/bp/blueprint_loader.js
+++ function/bp/blueprint_loader.js
@@ -50,7 +50,7 @@ function renderBlueprint(data, key) {
             <md-suggestion-chip class="md3-chip" id="bp-key" label="${data.req_game}" removable="false" selected="false"></md-suggestion-chip>
             <md-suggestion-chip class="md3-chip" id="bp-key" label="${data.req_type}" removable="false" selected="false"></md-suggestion-chip>
         </div>
-        <div>
+        <div div style="display: flex; gap: 8px;">
             <md-filled-tonal-button href="${data.file_link}" target="_blank"> Download
                 <svg slot="icon" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M480-337q-8 0-15-2.5t-13-8.5L308-492q-12-12-11.5-28t11.5-28q12-12 28.5-12.5T365-549l75 75v-286q0-17 11.5-28.5T480-800q17 0 28.5 11.5T520-760v286l75-75q12-12 28.5-11.5T652-548q11 12 11.5 28T652-492L508-348q-6 6-13 8.5t-15 2.5ZM240-160q-33 0-56.5-23.5T160-240v-80q0-17 11.5-28.5T200-360q17 0 28.5 11.5T240-320v80h480v-80q0-17 11.5-28.5T760-360q17 0 28.5 11.5T800-320v80q0 33-23.5 56.5T720-160H240Z"/></svg>
             </md-filled-tonal-button>
