428a1e039ba9a65cdb6315f7071cfdfa81af71ad
sflightx <spaceflightxploration@gmail.com>
2025-04-30 15:56:17 +0800

    Enhance CSS layout for banners and add responsive width for half banner; update Discord embed form to use static footer text

--- function/manifest/formDiscordEmbed.js
+++ function/manifest/formDiscordEmbed.js
@@ -5,9 +5,9 @@ document.getElementById("embedForm").addEventListener("submit", async function (
       title: document.getElementById("title").value,
       description: document.getElementById("description").value,
       color: document.getElementById("color").value,
-      footerText: document.getElementById("footerText").value,
       imageUrl: document.getElementById("imageUrl").value,
-      authorName: document.getElementById("authorName").value
+      authorName: "SFlightX",
+      footerText: "SFlightX | Launch Manifest",
     };
   
     const res = await fetch("/send-embed", {
