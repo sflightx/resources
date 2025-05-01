f189a1a110f0c1099658cd6059fa1a99019a6473
sflightx <spaceflightxploration@gmail.com>
2025-05-01 09:42:13 +0800

    Refactor Google authentication: import GoogleAuthProvider from initializeFirebase.js and streamline code structure

--- auth/v2/googleAuth.js
+++ auth/v2/googleAuth.js
@@ -1,10 +1,12 @@
-const provider = new firebase.auth.GoogleAuthProvider();
+import { auth, GoogleAuthProvider } from 'https://sflightx.com/resources/serviceAuth/initializeFirebase.js';
+
+const provider = new GoogleAuthProvider();
 
 function googleLogin() {
     auth.signInWithPopup(provider)
         .then((result) => {
             alert("Signed in as " + result.user.displayName);
-            window.location.href = "https://manifest.sflightx.com/discord/"; // or wherever you want
+            window.location.href = "https://manifest.sflightx.com/discord/";
         })
         .catch((error) => {
             document.getElementById("error-message").textContent = error.message;
--- serviceAuth/initializeFirebase.js
+++ serviceAuth/initializeFirebase.js
@@ -1,25 +1,27 @@
 // initializeFirebase.js
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-app.js';
-import { getAuth } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+import {
+  getAuth,
+  GoogleAuthProvider
+} from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
 import { getDatabase } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-database.js';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyATiu2dMGbYxGYH0S_31J8XsK5OBpTLMfQ",
-    authDomain: "sflight-x.firebaseapp.com",
-    databaseURL: "https://sflight-x-default-rtdb.firebaseio.com",
-    projectId: "sflight-x",
-    storageBucket: "sflight-x.appspot.com",
-    messagingSenderId: "219145189221",
-    appId: "1:219145189221:web:4a94931b99d15659fe138c",
-    measurementId: "G-813MEL2GZF"
+  apiKey: "AIzaSyATiu2dMGbYxGYH0S_31J8XsK5OBpTLMfQ",
+  authDomain: "sflight-x.firebaseapp.com",
+  databaseURL: "https://sflight-x-default-rtdb.firebaseio.com",
+  projectId: "sflight-x",
+  storageBucket: "sflight-x.appspot.com",
+  messagingSenderId: "219145189221",
+  appId: "1:219145189221:web:4a94931b99d15659fe138c",
+  measurementId: "G-813MEL2GZF"
 };
 
-// Initialize Firebase app
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Always call getAuth(app) and getDatabase(app) directly
 const auth = getAuth(app);
 const db = getDatabase(app);
 
-export { app, auth, db };
-export default app; // Export the app instance for use in other modules
+// Export GoogleAuthProvider so other modules can use it
+export { app, auth, db, GoogleAuthProvider };
+export default app;
