5d78f8c85531a412c66e34e4c04b82f5e3f1125c
sflightx <spaceflightxploration@gmail.com>
2025-05-01 10:13:38 +0800

    Refactor googleLogin function: replace auth.signInWithPopup with signInWithPopup import and change window behavior on success

--- auth/v2/googleAuth.js
+++ auth/v2/googleAuth.js
@@ -1,16 +1,22 @@
-import { auth, GoogleAuthProvider } from 'https://sflightx.com/resources/serviceAuth/initializeFirebase.js';
-
-const provider = new GoogleAuthProvider();
-
-function googleLogin() {
-    auth.signInWithPopup(provider)
-        .then((result) => {
-            alert("Signed in as " + result.user.displayName);
-            window.location.href = "https://manifest.sflightx.com/discord/";
-        })
-        .catch((error) => {
-            document.getElementById("error-message").textContent = error.message;
-        });
-}
-
-window.googleLogin = googleLogin;
+import {
+    auth,
+    GoogleAuthProvider
+  } from 'https://sflightx.com/resources/serviceAuth/initializeFirebase.js';
+  
+  import { signInWithPopup } from 'https://www.gstatic.com/firebasejs/10.8.0/firebase-auth.js';
+  
+  const provider = new GoogleAuthProvider();
+  
+  function googleLogin() {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        alert("Signed in as " + result.user.displayName);
+        window.close();
+      })
+      .catch((error) => {
+        document.getElementById("error-message").textContent = error.message;
+      });
+  }
+  
+  window.googleLogin = googleLogin;
+  
