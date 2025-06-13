3b6dacee6220e49d2005a380a4fdc3ecd52141f8
sflightx <spaceflightxploration@gmail.com>
2025-06-13 14:03:39 +0800

    added js support for dynamic toc

---
+++ v4/data/drawer/help_sflightx_com/help/404.js
@@ -0,0 +1,17 @@
+export const navData = [
+  {
+    href: "#step-1",
+    headline: "Introduction",
+    supportingText: "Learn about the app before proceeding"
+  },
+  {
+    href: "#step-2",
+    headline: "Getting Started",
+    supportingText: "Setting up the app"
+  },
+  {
+    href: "#step-3",
+    headline: "THIS IS A TEST",
+    supportingText: "Learn about the app before proceeding"
+  }
+];
