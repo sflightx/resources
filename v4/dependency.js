ee6901ab8cb92e27e0ece8b985a79bc76dde5bcb
Edrick <caidoedrickjohn@gmail.com>
2026-09-25 21:50:44 +0800

    update script paths in dependency.js to use absolute URLs

--- v4/dependency.js
+++ v4/dependency.js
@@ -1,9 +1,9 @@
 const scripts = [
-  "../resources/v4/function/drawer.js",
-  "../resources/v4/function/theme-mode.js",
-  "../resources/v4/function/footer.js",
-  "../resources/v4/data/drawer/nav-loader.js",
-  "../resources/v4/function/element/dialog.js"
+  "https://sflightx.com/resources/v4/function/drawer.js",
+  "https://sflightx.com/resources/v4/function/theme-mode.js",
+  "https://sflightx.com/resources/v4/function/footer.js",
+  "https://sflightx.com/resources/v4/data/drawer/nav-loader.js",
+  "https://sflightx.com/resources/v4/function/element/dialog.js"
 ];
 
 scripts.forEach(src => {
