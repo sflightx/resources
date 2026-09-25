d5260405b4a8c8a4fb5789d708230a20641d1a9d
Edrick <caidoedrickjohn@gmail.com>
2026-09-25 19:41:44 +0800

    refactor the whole website 1

--- v4/dependency.js
+++ v4/dependency.js
@@ -1,16 +1,15 @@
 const scripts = [
-    "https://sflightx.com/resources/v4/function/drawer.js",
-    "https://sflightx.com/resources/v4/function/scroll.js",
-    "https://sflightx.com/resources/v4/function/theme-mode.js",
-    "https://sflightx.com/resources/v4/function/footer.js",
-    "https://sflightx.com/resources/v4/data/drawer/nav-loader.js",
-    "https://sflightx.com/resources/v4/function/element/dialog.js"
-  ];
+  "../resources/v4/function/drawer.js",
+  "../resources/v4/function/theme-mode.js",
+  "../resources/v4/function/footer.js",
+  "../resources/v4/data/drawer/nav-loader.js",
+  "../resources/v4/function/element/dialog.js"
+];
 
-  scripts.forEach(src => {
-    const script = document.createElement("script");
-    script.src = src;
-    script.type = "module";
-    script.defer = true;
-    document.head.appendChild(script);
-  });
+scripts.forEach(src => {
+  const script = document.createElement("script");
+  script.src = src;
+  script.type = "module";
+  script.defer = true;
+  document.head.appendChild(script);
+});
